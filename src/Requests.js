22fa560ecf6708dae9d88245c47136eb93a847e6
Nithin <58898276+NithinKaala98@users.noreply.github.com>
2023-07-30 23:01:00 +0530

    Add files via upload

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+
+const key = '7b85f25937dd2978d9735b2f64ffa84a';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
